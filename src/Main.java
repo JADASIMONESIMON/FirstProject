ed3aea8ebf18d28aeae8258dcbc8942267433ba0
jadas <jadasssimon@gmail.com>
2023-09-04 22:55:27 -0400

    I create to only print salary.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main( String [] args) {
+        int age = 25;
+        double salary = 50000.0;
+        String name = "John";
+        System.out.print(salary);
+    }
+}
